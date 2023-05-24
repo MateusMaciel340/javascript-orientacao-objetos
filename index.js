d721fcdd1dbbfe6e622962324d8a72fbeb924633
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-24 10:13:53 -0300

    feat: adicionando comportamentos

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
-    rg;
 }
 
-const primeiroCliente = new Cliente();
+class ContaCorrente {
+    agencia;
+    _saldo = 0;
 
-// Primeiro Cliente
-primeiroCliente.nome = 'Ricardo';
-primeiroCliente.cpf = 11111111111;
-primeiroCliente.agencia = 1001;
-primeiroCliente.saldo = 0;
-primeiroCliente.rg = 123456789;
+    sacar(valor) {
+        
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    }
 
-const segundoCliente = new Cliente();
+    depositar(valor) {
 
-// Segundo Cliente
-segundoCliente.nome = 'Alice';
-segundoCliente.cpf= 22222222222;
-segundoCliente.agencia = 1001;
-segundoCliente.saldo = 0;
-segundoCliente.rg = 12345678;
+        if (valor <= 0) {
+            return;
+        }
 
-console.log(primeiroCliente);
-console.log(segundoCliente);
+        this._saldo += valor;
+    }
+}
+
+// Cliente Ricardo
+const clienteRicardo = new Cliente();
+clienteRicardo.nome = 'Ricardo';
+clienteRicardo.cpf = 11111111111;
+
+// Cliente Alice
+const clienteAlice = new Cliente();
+clienteAlice.nome = 'Alice';
+clienteAlice.cpf = 22222222222;
+
+// Conta Corrente do Cliente Ricardo
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo._saldo = 1000;
+
+// Depositos do Cliente Ricardo
+contaCorrenteRicardo.depositar(300);
+
+// Valor Sacado
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+// Chamando Conta Corrente do Ricardo com Alterações
+console.log(contaCorrenteRicardo);
