d95b08c3d4647524998e033891c670fed72bf3cd
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-05-26 14:18:31 -0300

    feat: sistema interno

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import { Cliente } from './classes/Cliente.js';
-import { ContaCorrente } from './classes/ContaCorrente.js';
-import { ContaPoupanca } from './classes/ContaPoupanca.js';
-import { ContaSalario } from './classes/ContaSalario.js';
+import { Cliente } from './Classes/Cliente.js';
+import { Gerente } from './Funcionarios/Gerente.js';
+import { Diretor } from './Funcionarios/Diretor.js';
+import { SistemaAutenticacao } from './Classes/SistemaAutenticacao.js';
 
-// Cliente Ricardo
-const clienteRicardo = new Cliente('Ricardo', 11111111111);
+// Diretor Rodrigo
+const diretorRodrigo = new Diretor('Rodrigo', 1000, 123456789);
+diretorRodrigo.cadastrarSenha('12345678');
+const statusLogadoDiretor = SistemaAutenticacao.login(diretorRodrigo, '12345678');
 
-// Conta Corrente do Ricardo
-const contaCorrenteRicardo = new ContaCorrente(clienteRicardo, 1001);
+// Gerente Ricardo
+const gerenteRicardo = new Gerente('Ricardo', 5000, 123456789);
+gerenteRicardo.cadastrarSenha('1234');
+const statusLogadoGerente = SistemaAutenticacao.login(gerenteRicardo, '1234');
 
-// Conta Poupança do Ricardo
-const contaPoupancaRicardo = new ContaPoupanca(50, clienteRicardo, 1001);
+// Cliente
+const clienteLais = new Cliente('Lais', 123456789, '4002');
+const statusLogadoCliente = SistemaAutenticacao.login(clienteLais, '4002');
 
-// Conta Salário
-const contaSalario = new ContaSalario(clienteRicardo);
-contaSalario.depositar(100);
-contaSalario.sacar(50);
-
-console.log(contaSalario);
+// Verificação de Status
+console.log(statusLogadoDiretor);
+console.log(statusLogadoGerente);
+console.log(statusLogadoCliente);
